c5b753a7653e51849dfa84a935b437a733330cea
Léonard Lemaire <leonard.lemaire@epfl.ch>
2025-04-10 16:07:09 +0200

    Implement logistic regression and Implement part of the main program
    I implemented the LogisticRegression class and implemented part of the main program, that is the logistic regression part as well as the data preprocessing and finally the time analysis logging

--- 380620_357850_360968_project/main.py
+++ 380620_357850_360968_project/main.py
@@ -9,6 +9,7 @@ from src.methods.knn import KNN
 from src.methods.kmeans import KMeans
 from src.utils import normalize_fn, append_bias_term, accuracy_fn, macrof1_fn, mse_fn
 import os
+import time
 
 np.random.seed(100)
 
@@ -38,10 +39,19 @@ def main(args):
     ## 2. Then we must prepare it. This is where you can create a validation set, normalize, add bias, etc.
     # Make a validation set (it can overwrite xtest, ytest)
     if not args.test:
-        ### WRITE YOUR CODE HERE
-        pass
+        fraction_train = 0.8 #New training set is 80% of the samples of the original training set, 20% remaining are validation set
+        num_samples = xtrain.shape[0]
+        rinds = np.random.permutation(num_samples)
+        n_train = int(num_samples * fraction_train)
+        xtest = xtrain[rinds[n_train:]] 
+        ytest = ytrain[rinds[n_train:]] 
 
-    ### WRITE YOUR CODE HERE to do any other data processing
+    #Standardizing data for logistic regression, k-means, knn
+    if args.method == "logistic_regression" or args.method == "kmeans" or args.method == "knn":
+        mean = np.mean(xtrain, axis=0)
+        std = np.std(xtrain, axis=0)
+        xtrain = (xtrain - mean) / std
+        xtest = (xtest - mean) / std
 
     ## 3. Initialize the method you want to use.
 
@@ -53,24 +63,32 @@ def main(args):
     if args.method == "dummy_classifier":
         method_obj = DummyClassifier(arg1=1, arg2=2)
 
-    elif ...:  ### WRITE YOUR CODE HERE
-        pass
+    elif args.method == "logistic_regression":
+        method_obj = LogisticRegression(args.lr, args.max_iters)
+
+    s1 = time.time() #First time checkpoint
 
     ## 4. Train and evaluate the method
     # Fit (:=train) the method on the training data for classification task
     preds_train = method_obj.fit(xtrain, ytrain)
+    
+    s2 = time.time() #Second time checkpoint
 
     # Predict on unseen data
     preds = method_obj.predict(xtest)
+    
+    s3 = time.time() #Third time checkpoint
 
     # Report results: performance on train and valid/test sets
     acc = accuracy_fn(preds_train, ytrain)
     macrof1 = macrof1_fn(preds_train, ytrain)
+    print(f"Training {args.method} took {s2 - s1} seconds")
     print(f"\nTrain set: accuracy = {acc:.3f}% - F1-score = {macrof1:.6f}")
 
     acc = accuracy_fn(preds, ytest)
     macrof1 = macrof1_fn(preds, ytest)
-    print(f"Test set:  accuracy = {acc:.3f}% - F1-score = {macrof1:.6f}")
+    print(f"Predicting using {args.method} took {s3 - s2} seconds")
+    print(f"{"Test" if not args.test else "Validation"} set:  accuracy = {acc:.3f}% - F1-score = {macrof1:.6f}")
 
     ### WRITE YOUR CODE HERE if you want to add other outputs, visualization, etc.
 
@@ -86,7 +104,7 @@ if __name__ == "__main__":
         help="dummy_classifier / knn / logistic_regression / kmeans / nn (MS2)",
     )
     parser.add_argument(
-        "--data_path", default="data", type=str, help="path to your dataset"
+        "--data_path", default="../Data_MS1_2025", type=str, help="path to your dataset"
     )
     parser.add_argument(
         "--data_type", default="features", type=str, help="features/original(MS2)"
--- 380620_357850_360968_project/src/methods/logistic_regression.py
+++ 380620_357850_360968_project/src/methods/logistic_regression.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from ..utils import get_n_classes, label_to_onehot, onehot_to_label
+from ..utils import get_n_classes, label_to_onehot, onehot_to_label, accuracy_fn
 
 
 class LogisticRegression(object):
@@ -10,7 +10,7 @@ class LogisticRegression(object):
 
     def __init__(self, lr, max_iters=500):
         """
-        Initialize the new object (see dummy_methods.py)
+        Initialize the new object
         and set its arguments.
 
         Arguments:
@@ -31,12 +31,20 @@ class LogisticRegression(object):
         Returns:
             pred_labels (array): target of shape (N,)
         """
-        ##
-        ###
-        #### WRITE YOUR CODE HERE!
-        ###
-        ##
-        return pred_labels
+        D = training_data.shape[1]  # number of features
+        C = get_n_classes(training_labels)  # number of classes
+        onehot_labels = label_to_onehot(training_labels, C)
+        self.weights = np.random.normal(0, 0.1, (D, C))
+        for it in range(self.max_iters):
+            gradient = self.gradient_logistic_multi(training_data, onehot_labels)
+            self.weights = self.weights - self.lr * gradient
+
+            predictions = self.predict(training_data)
+            
+            if accuracy_fn(predictions, training_labels) == 100:
+                break
+        
+        return predictions
 
     def predict(self, test_data):
         """
@@ -47,9 +55,35 @@ class LogisticRegression(object):
         Returns:
             pred_labels (array): labels of shape (N,)
         """
-        ##
-        ###
-        #### WRITE YOUR CODE HERE!
-        ###
-        ##
-        return pred_labels
+        softmaxed_data = self.f_softmax(test_data, self.weights)
+        return np.argmax(softmaxed_data, 1)
+    
+    def f_softmax(self, data):
+        """
+        Softmax function for multi-class logistic regression.
+        
+        Args:
+            data (array): Input data of shape (N, D)
+            W (array): Weights of shape (D, C) where C is the number of classes
+        Returns:
+            array of shape (N, C): Probability array where each value is in the
+                range [0, 1] and each row sums to 1.
+                The row i corresponds to the prediction of the ith data sample, and 
+                the column j to the jth class. So element [i, j] is P(y_i=k | x_i, W)
+        """
+        exp_distance = np.exp(data @ self.weights)
+        sum_terms = np.sum(exp_distance, 1)
+        return exp_distance / sum_terms[:, np.newaxis]
+    
+    def gradient_logistic_multi(self, data, labels):
+        """
+        Compute the gradient of the entropy for multi-class logistic regression.
+        
+        Args:
+            data (array): Input data of shape (N, D)
+            labels (array): Labels of shape  (N, C)  (in one-hot representation)
+            W (array): Weights of shape (D, C)
+        Returns:
+            grad (np.array): Gradients of shape (D, C)
+        """
+        return data.T @ (self.f_softmax(data, self.weights) - labels)
