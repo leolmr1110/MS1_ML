Add optimal hyperparameter search and data preprocessing for logistic regression, Fix bug in softmax function

380620_357850_360968_project/main.py
@@ -10,8 +10,60 @@ from src.methods.kmeans import KMeans
 from src.utils import normalize_fn, append_bias_term, accuracy_fn, macrof1_fn, mse_fn
 import os
 import time
+import matplotlib.pyplot as plt
 
 np.random.seed(100)
+
+
+def choose_hyperparameter_logistic_regression(data, test_data, labels, test_labels):
+    nb_lr = 6
+    nb_max_iter = 25
+    learning_rates = np.logspace(-6, -1, num=nb_lr)
+    max_iterations = np.linspace(100, 10_000, num=nb_max_iter).astype(int)
+    print(f"Learning rates: {learning_rates}")
+    print(f"Max Iterations: {max_iterations}")
+    
+    #creates 2 matrices with the values of the cross product of the lr and mi
+    expanded_lr, expanded_mi = np.meshgrid(learning_rates, max_iterations)
+    #we flatten the 2 matrices created above and combine them to finish the cross product
+    coordinates_2d = np.column_stack([expanded_lr.ravel(), expanded_mi.ravel()])
+    
+    #third column to be filled with the loop results
+    third_column = np.zeros((nb_max_iter * nb_lr, 1))
+    
+    #3D coordinates to plot and choose the best hyperparameters
+    accuracy_results = np.hstack([coordinates_2d, third_column])
+    macrof1_results = np.hstack([coordinates_2d, third_column])
+    
+    for i in range(nb_lr):
+        for j in range(nb_max_iter):
+            logistic_regression = LogisticRegression(learning_rates[i], max_iterations[j])
+            logistic_regression.fit(data, labels)
+            preds = logistic_regression.predict(test_data)
+            acc = accuracy_fn(preds, test_labels)
+            macrof1 = macrof1_fn(preds, test_labels)
+            accuracy_results[i * nb_max_iter + j, 2] = acc
+            macrof1_results[i * nb_max_iter + j, 2] = macrof1
+            
+    fig = plt.figure()
+    
+    log_learning_rates = np.log10(accuracy_results[:, 0])
+    log_macrof1 = np.log10(macrof1_results[:, 0])
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(log_learning_rates, accuracy_results[:, 1], accuracy_results[:, 2], c='blue', marker='o', label='Accuracy')
+    ax.scatter(log_macrof1, macrof1_results[:, 1], macrof1_results[:, 2] * 100, c='red', marker='x', label='Macro F1')
+    ax.set_xlabel("Learning rate (log10)")
+    ax.set_ylabel("Max Iterations")
+    ax.set_zlabel("Score (%)")
+    ax.grid(True)
+    
+    plt.legend()
+    plt.show()
+    
+    accuracy_extract = accuracy_results[:, 2]
+    macrof1_extract = macrof1_results[:, 2]
+    best_hyperparameters = np.lexsort((macrof1_extract, accuracy_extract))[-1]
+    return np.hstack([accuracy_results[best_hyperparameters], macrof1_results[best_hyperparameters, 2]])
 
 
 def main(args):
@@ -46,12 +98,17 @@ def main(args):
         xtest = xtrain[rinds[n_train:]] 
         ytest = ytrain[rinds[n_train:]] 
 
-    #Standardizing data for logistic regression, k-means, knn
+    #Normalizing data for logistic regression, k-means, knn
     if args.method == "logistic_regression" or args.method == "kmeans" or args.method == "knn":
         mean = np.mean(xtrain, axis=0)
         std = np.std(xtrain, axis=0)
-        xtrain = (xtrain - mean) / std
-        xtest = (xtest - mean) / std
+        xtrain = normalize_fn(xtrain, mean, std)
+        xtest = normalize_fn(xtest, mean, std)
+    
+    #Adding a bias term for logistic regression
+    if args.method == "logistic_regression":
+        xtrain = append_bias_term(xtrain)
+        xtest = append_bias_term(xtest)
 
     ## 3. Initialize the method you want to use.
 
@@ -59,7 +116,6 @@ def main(args):
     if args.method == "nn":
         raise NotImplementedError("This will be useful for MS2.")
 
-    # Follow the "DummyClassifier" example for your methods
     if args.method == "dummy_classifier":
         method_obj = DummyClassifier(arg1=1, arg2=2)
 
@@ -81,7 +137,7 @@ def main(args):
     preds = method_obj.predict(xtest)
     
     s3 = time.time() #Third time checkpoint
-
+    
     # Report results: performance on train and valid/test sets
     acc = accuracy_fn(preds_train, ytrain)
     macrof1 = macrof1_fn(preds_train, ytrain)
@@ -91,9 +147,14 @@ def main(args):
     acc = accuracy_fn(preds, ytest)
     macrof1 = macrof1_fn(preds, ytest)
     print(f"Predicting using {args.method} took {s3 - s2} seconds")
-    print(f"{"Test" if not args.test else "Validation"} set:  accuracy = {acc:.3f}% - F1-score = {macrof1:.6f}")
+    print(f"{'Test' if args.test else 'Validation'} set:  accuracy = {acc:.3f}% - F1-score = {macrof1:.6f}")
 
     ### WRITE YOUR CODE HERE if you want to add other outputs, visualization, etc.
+    
+    # if args.method == "logistic_regression":
+    #     optimal_parameters = choose_hyperparameter_logistic_regression(xtrain, xtest, ytrain, ytest)
+    #     print(f"The optimal values for the hyperparameters are attained for lr={optimal_parameters[0]} and max_iter={optimal_parameters[1]}")
+    #     print(f"For the following optimal values: accuracy={optimal_parameters[2]} and macrof1={optimal_parameters[3]}")
 
 
 if __name__ == "__main__":

380620_357850_360968_project/src/methods/logistic_regression.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from ..utils import get_n_classes, label_to_onehot, onehot_to_label, accuracy_fn
+from ..utils import get_n_classes, label_to_onehot, accuracy_fn
 
 
 class LogisticRegression(object):
@@ -86,4 +86,4 @@ class LogisticRegression(object):
         Returns:
             grad (np.array): Gradients of shape (D, C)
         """
-        return data.T @ (self.f_softmax(data, self.weights) - labels)
+        return data.T @ (self.f_softmax(data) - labels)